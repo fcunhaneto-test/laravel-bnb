173f9ecbe0f80ea8073230f1d20829adde0fba35
Francisco Cunha Neto <fcunhaneto@gmail.com>
2020-02-29 15:42:56 -0300

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from "vue-router";
+import router from "./routes";
+
+Vue.use(VueRouter);
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-2', require('./components/Example2.vue').default);
+
+const app = new Vue({
+    router,
+    el: '#app',
+});
