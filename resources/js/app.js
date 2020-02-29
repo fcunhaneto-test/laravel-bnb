dc7b4aca71444408f1145e03cf8c2b2813bc1fa7
Francisco Cunha Neto <fcunhaneto@gmail.com>
2020-02-29 15:55:29 -0300

    Criado Index.vue como componente principal.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,16 @@ window.Vue = require('vue');
 import VueRouter from "vue-router";
 import router from "./routes";
 
+import Index from "./components/Index";
 Vue.use(VueRouter);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-2', require('./components/Example2.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-2', require('./components/Example2.vue').default);
 
 const app = new Vue({
     router,
     el: '#app',
+    components: {
+        'index': Index
+    }
 });
